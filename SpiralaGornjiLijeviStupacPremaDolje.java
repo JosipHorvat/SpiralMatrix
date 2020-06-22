6786ac0bc7619872f5852994d20560c70fdc8077
Josip Horvat <josiph988@gmail.com>
2020-06-22 16:38:06 +0100

    Druga inacica spiralne matrice.

---
+++ SpiralaGornjiLijeviStupacPremaDolje.java
@@ -0,0 +1,60 @@
+package petlje;
+
+
+
+public class SpiralaGornjiLijeviStupacPremaDolje {
+	public static void main(String[] args) {
+		  
+	           
+	            int broj1 =5;    
+	            int broj2 = 5;    
+	            
+	            int [][] spirala = new int[ broj1][ broj2];
+	            
+	            
+	            int vrijednost=1;
+	            int minStupac=0;
+	            int minRed=0;
+	            int maxStupac=broj1-1;
+	            int maxRed= broj2-1;
+	           
+	 
+	            while(vrijednost<= broj1* broj2)
+	                {
+	            	for (int i = minRed; i<= maxRed; i++) {
+	            		spirala[i][minStupac] = vrijednost;
+	            		vrijednost++;
+	            	}
+	            	for(int i = minStupac +1; i<=maxStupac; i++) {
+	            		spirala[maxRed][i] = vrijednost;
+	            		vrijednost++;
+	            	}
+	            	for(int i = maxRed -1; i>=minRed; i-- ) {
+	            		spirala[i][maxStupac] = vrijednost;
+	            		vrijednost++;
+	            	}
+	            	for(int i = maxStupac -1; i>minStupac; i-- ) {
+	            		spirala[minRed][i] = vrijednost;
+	            		vrijednost++;
+	            		
+	            	}
+	            	
+	                   	 
+	                 minStupac++;
+	                 maxStupac--;
+	                 minRed++;
+	                 maxRed--;
+	                }
+	   
+	            
+	            System.out.println("Spirala sa gornje lijeve strane prema dolje:");
+	            for(int i=0;i<broj1;i++)
+	                {
+	                    for(int j=0;j<broj2;j++)
+	                        {
+	                            System.out.printf( "%5d", spirala[i][j]);
+	                        }
+	                 System.out.println();
+	                }
+	        }
+	}
